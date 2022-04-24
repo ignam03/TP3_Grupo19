ffcf1c376015929f8eaa5d6bccb481fb55f36f41
ignam03 <ignafree97@gmail.com>
2022-04-24 17:48:05 -0300

    ejercicio4 realizado

---
+++ TP3_Grupo19/src/main/java/ar/edu/unju/fi/ejercicio4/Principal.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] listaNumero = new int [8];
+		int num;
+		
+		for(int i = 0; i < listaNumero.length;i++) {
+			System.out.println("Ingrese un numero");
+			Scanner teclado = new Scanner(System.in);
+			num = teclado.nextInt();
+			listaNumero[i] = num;
+		}
+		
+
+		for(int j = 0; j < listaNumero.length; j++) {
+			if(listaNumero[j] % 2 == 0) {
+				System.out.println(listaNumero[j] + " es par");
+			} else {
+				System.out.println(listaNumero[j] + " es impar");
+			}
+		}
+	}
+
+}
