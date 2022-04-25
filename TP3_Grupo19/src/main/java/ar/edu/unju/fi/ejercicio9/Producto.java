7bba4192ee7f33bd4f1ce565cfa54beb4931045f
ignam03 <ignafree97@gmail.com>
2022-04-24 22:36:06 -0300

    ejercicio9 emepezado

---
+++ TP3_Grupo19/src/main/java/ar/edu/unju/fi/ejercicio9/Producto.java
@@ -0,0 +1,46 @@
+package ar.edu.unju.fi.ejercicio9;
+
+public class Producto {
+	private int codigo;
+	private String descripcion;
+	private double precio;
+	private String marca;
+	
+	public Producto() {
+		
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+}
